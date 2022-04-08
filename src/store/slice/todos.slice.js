3acacae347bb89ed118ac4f269d578c722b50d75
IgorVykhovanets <igorvykhovanets808@gmail.com>
2022-04-08 16:03:30 +0300

    base commit

--- src/store/slice/todos.slice.js
+++ src/store/slice/todos.slice.js
@@ -7,14 +7,10 @@ const todosSlice = createSlice({
     },
     reducers: {
         addTodo: (state, action) => {
-            let todo = {id: new Date().getTime(), todo: action.payload.todo, status: false}
-            localStorage.setItem(todo.id, JSON.stringify(todo));
-            let item = localStorage.getItem(todo.id);
-            state.todos.push(JSON.parse(item));
+            state.todos.push({id: new Date().getTime(), todo: action.payload.todo, status: false});
         },
         deleteTodo: (state, action) => {
             state.todos = state.todos.filter(todo => todo.id !== action.payload.id);
-            localStorage.removeItem(toString(state.todos.id));
         },
         changeStatus: (state, action) => {
             let todo = state.todos.find(todo => todo.id === action.payload.id);
